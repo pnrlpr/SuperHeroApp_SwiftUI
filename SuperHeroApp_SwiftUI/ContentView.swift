14b1b1264e8a13bac5b00e80b4c1752076494260
pinaralper <ppinaralper@gmail.com>
2023-09-22 13:25:37 +0300

    Initial Commit

---
+++ SuperHeroApp_SwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SuperHeroApp_SwiftUI
+//
+//  Created by PINAR KALKAN on 22.09.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SuperHeroApp_SwiftUI/SuperHeroApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroApp_SwiftUIApp.swift
+//  SuperHeroApp_SwiftUI
+//
+//  Created by PINAR KALKAN on 22.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
